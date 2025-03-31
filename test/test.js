a5559c0785cf3bb7a1a4cc0a33fa7337c4f77cc0
Nester <nesterkabatuku389@gmail.com>
2025-03-31 20:00:44 +0300

    added several tests, but one test failed, multiplying a character with a number

--- test/test.js
+++ test/test.js
@@ -3,18 +3,36 @@ import multiply from "../multiply.js";
 
 describe("Multiply Function", function () {
   it("should return 1 when multiplying 1 * 1", function () {
-    assert.equal(multiply(1, 1), 1);
+    assert.strictEqual(multiply(1, 1), 1);
   });
   it("should return 4 when multiplying 2 * 2", function () {
-    assert.equal(multiply(2, 2), 4);
+    assert.strictEqual(multiply(2, 2), 4);
   });
   it("should return 9 when multiplying 3 * 3", function () {
-    assert.equal(multiply(3, 3), 9);
+    assert.strictEqual(multiply(3, 3), 9);
   });
-  it("should return 16 when multiplying 4 * 4", function () {
-    assert.equal(multiply(4, 4), 16);
+  it("should return 1035 when multiplying 23* 45", function () {
+    assert.strictEqual(multiply(23, 45), 1035);
   });
-  it("should return 1035 when multiplying 23 * 45", function () {
-    assert.equal(multiply(23, 45), 1035);
+  it("should return 0 when multiplying 0 * 100", function () {
+    assert.strictEqual(multiply(0, 100), 0);
+  });
+  // it("should return -9 when multiplying -9 * 1", function () {
+  //   assert.equal(multiply(-9, 1), -10);
+  // });
+  it("should return 4 when multiplying -4 * -1", function () {
+    assert.strictEqual(multiply(-4, -1), 4);
+  });
+  it("should return 4 when multiplying -4 * -1", function () {
+    assert.strictEqual(multiply(-4, -1), 4);
+  });
+  it('should return 4 when multiplying  "1" * 4', function () {
+    assert.strictEqual(multiply("1", 4), 4);
+  });
+  it('should return 8 when multiplying  "2" * "4"', function () {
+    assert.strictEqual(multiply("2", "4"), 8);
+  });
+  it("should return NaN when multiplying a character by a number", () => {
+    assert.strictEqual(multiply("a", 2), NaN);
   });
 });
