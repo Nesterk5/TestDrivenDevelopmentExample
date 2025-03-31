1e624a8f8f8fd3984665961efd220b6e19f7f1b3
Nester <nesterkabatuku389@gmail.com>
2025-03-31 18:57:25 +0300

    added more three tests

--- test/test.js
+++ test/test.js
@@ -8,4 +8,13 @@ describe("Multiply Function", function () {
   it("should return 4 when multiplying 2 * 2", function () {
     assert.equal(multiply(2, 2), 4);
   });
+  it("should return 9 when multiplying 3 * 3", function () {
+    assert.equal(multiply(3, 3), 9);
+  });
+  it("should return 16 when multiplying 4 * 4", function () {
+    assert.equal(multiply(4, 4), 16);
+  });
+  it("should return 1035 when multiplying 23 * 45", function () {
+    assert.equal(multiply(23, 45), 1035);
+  });
 });
