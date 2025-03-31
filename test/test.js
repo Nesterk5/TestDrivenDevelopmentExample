33953b64e082c1cbd2e8d1785f1fdec2c596b75f
Nester <nesterkabatuku389@gmail.com>
2025-03-31 18:52:40 +0300

    TDD Cycle 2 -added a new test- test 2 which is expected to fail first whereas test 1 is expected to pass

--- test/test.js
+++ test/test.js
@@ -5,4 +5,7 @@ describe("Multiply Function", function () {
   it("should return 1 when multiplying 1 * 1", function () {
     assert.equal(multiply(1, 1), 1);
   });
+  it("should return 4 when multiplying 2 * 2", function () {
+    assert.equal(multiply(2, 2), 4);
+  });
 });
